0a47b7ae1fc30cb285072d334c07dc150bfbdfce
Emi-vw <emiya.vanwyk@gmail.com>
2023-03-29 10:22:36 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-createApp(App).use(store).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyDiAdMhOkD-Ihyx_clQzobRfawPCnKTtCM",
+  authDomain: "atmovie-307d0.firebaseapp.com",
+  projectId: "atmovie-307d0",
+  storageBucket: "atmovie-307d0.appspot.com",
+  messagingSenderId: "568165041236",
+  appId: "1:568165041236:web:abe974adc8efb9e29619c1",
+  measurementId: "G-37Z18NTRQ5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
